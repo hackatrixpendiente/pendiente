2ba122aae0871a1549687fe04df7ecf515b66e78
francobko <franco.condor.urp@gmail.com>
2019-06-01 13:03:04 -0500

    app login

--- app/app/src/main/java/com/example/pendienteapp/LoginActivity.java
+++ app/app/src/main/java/com/example/pendienteapp/LoginActivity.java
@@ -1,13 +1,26 @@
 package com.example.pendienteapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
+    Button btnlogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        btnlogin = findViewById(R.id.button);
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), MainActivity.class);
+                startActivityForResult(intent, 0);
+            }
+        });
+
     }
 }
