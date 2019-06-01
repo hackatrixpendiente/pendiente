b9cba0b1d44f4cf652046bd165d503c9dbd3c67e
francobko <franco.condor.urp@gmail.com>
2019-06-01 14:01:56 -0500

    seleccion

--- API/pendienteapi/routes/pendiente.js
+++ API/pendienteapi/routes/pendiente.js
@@ -4,72 +4,32 @@ var router = express.Router();
 var pg = require('pg');
 var dbConfig = require('../public/javascripts/database');
 
-router.post('/', function(req, res, next) { // lista de pedidos segun lab(idaula) y usuario(idusuario)
-    var datos = req.body;
-    var client = new pg.Client(dbConfig);
-    client.connect();
-    const query = {
-        text: `SELECT * 
-                 FROM "PENDIENTE" p 
-                WHERE p."IDUSUARIO" = $2 AND p."IDAULA" = $1`,
-        values: [datos.idaula,datos.idusuario],
+/* GET users listing. */
+router.post('/', function(req, res, next) {
+  var datos = req.body;
+  var client = new pg.Client(dbConfig);
+  client.connect();
+  const query = {
+    text: `SELECT *
+             FROM "PENDIENTE" p1
+            WHERE p1."FLGCATEGORIA" = $1`,
+    values: [datos.idcategoria],
+  }
+  client.query(query, (err,rows)=>{
+    if(err) {
+      console.log(err);
+      res.status(500).json({
+          success: false,
+          msg: 'Error'
+      })
+    } else {
+      res.status(200).json({
+        succes: true,
+        msg: 'Success',
+        data: rows.rows
+      });
     }
-    client.query(query, (err,rows)=>{
-        if(err) {
-            console.log(err);
-            res.status(500).json({
-                success: false,
-                msg: 'Error'
-            })
-        } else {
-            if(rows.rows[0] != null){
-                res.status(200).json({
-                    succes: true,
-                    msg: 'Success',
-                    data: rows.rows
-                });
-            } else {
-                res.status(200).json({
-                    succes: true,
-                    msg: 'Success'
-                });
-            }
-        }
-    });
-});
-
-router.post('/producto/', function(req, res, next) { //aulas del usuario
-    var datos = req.body;
-    var client = new pg.Client(dbConfig);
-    client.connect();
-    const query = {
-        text: `SELECT *
-                 FROM "PENDIENTEPRODUCTO"
-                WHERE "IDEMPRESA" = $1`,
-        values: [datos.idempresa],
-    }
-    client.query(query, (err,rows)=>{
-        if(err) {
-          console.log(err);
-          res.status(500).json({
-              success: false,
-              msg: 'Error'
-          })
-        } else {
-          if(rows.rows[0] != null){
-              res.status(200).json({
-                  succes: true,
-                  msg: 'Success',
-                  data: rows.rows
-              });
-          } else {
-              res.status(200).json({
-                  succes: true,
-                  msg: 'Success'
-              });
-          }
-        }
-    });
+  });
 });
 
 module.exports = router;
